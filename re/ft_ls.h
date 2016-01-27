0c4722dd7ca12f1c1da8d9601d2d856c13ffa735
Arthur DU PELOUX <adu-pelo@e1r1p12.42.fr>
2016-01-27 18:18:51 +0100

    re

--- re/ft_ls.h
+++ re/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: adu-pelo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/09 14:59:58 by adu-pelo          #+#    #+#             */
-/*   Updated: 2016/01/20 18:43:41 by adu-pelo         ###   ########.fr       */
+/*   Updated: 2016/01/27 18:18:37 by adu-pelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include <dirent.h> // opendir, readdir, closedir
 # include <sys/stat.h> // stat, lstat
 # include <pwd.h> // getpwuid
-// # include <uuid/uuid.h> // getpwuid, getgrgid
+# include <uuid/uuid.h> // getpwuid, getgrgid
 # include <grp.h> // getgrgid
 # include <sys/xattr.h> // listxattr, getxattr
 # include <time.h> // time, ctime
@@ -33,7 +33,7 @@ typedef struct 		s_opt // stock input opt
 	int			a;
 	int			r;
 	int			t;
-	char			err;
+	char		err;
 }					t_opt;
 
 typedef struct		s_file // stock file info
@@ -42,14 +42,17 @@ typedef struct		s_file // stock file info
 	char			*user_id; // user name of file
 	char			*group_id; // group name of file
 	char			*date; // date of modif of file
-	int			size; // size of file
+	int				size; // size of file
 }					t_info;
 
 typedef struct		s_lst
 {
-	char			*name;
+	//char			*name;
 	char 			*path;
 	struct s_lst 	*next;
 }					t_lst;
+
+int		get_opt(char *path, t_opt *opt);
+void	get_param(char *str);
 
 #endif
--- re/main.c
+++ re/main.c
@@ -1,6 +1,14 @@
-//
-// HEADER
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adu-pelo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/27 10:16:35 by adu-pelo          #+#    #+#             */
+/*   Updated: 2016/01/27 14:50:48 by adu-pelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "ft_ls.h"
 #include <stdio.h> // test
@@ -9,20 +17,27 @@ int	main(int ac, char **av)
 {
 	int i;
 	t_list *arg;
-	t_opt *opt;
+	t_opt opt;
 
-	if (ac)
+	if (ac > 1)
 	{
 		i = 1;
-		if (av[i][0] == '-' && av[i][1])
-			if (get_opt(av[i]))
+		if (av[i][0] == '-' && av[i][1]) // si av[1] commence par -x, on check les opt
+		{
+			if (get_opt(av[i], &opt)) // si une opt est invalide, return 1
 				return (1);
-		while (av[i++])
+			i = 2;
+		}
+		while (av[i])
+		{
 			get_param(av[i]);
+			i++;
+		}
 	}
 	else
 	{
 		printf("--- AC = 0 ---\n");
+		get_param(".");
 		return (0);
 	}
 	return (0);
